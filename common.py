db0b99d3afdd451ae3036ed7de71c5aedfd4cce5
Ming.G <mjzshd@gmail.com>
2012-09-07 12:08:20 +0800

    Server done~!

--- common.py
+++ common.py
@@ -1,6 +1,18 @@
 import socket
 def decodeRecv(sock, num = 8):
-	data = sock.recv(num);
+	data = sock.recv(num)
 	i = num - 2
 	while i >= 0:
 		data[i] = data[i] ^ data[i + 1]
+	return data
+def encodeSend(sock, data, num = 8):
+	for i in xrange(len(data), num):
+		for j in xrange(num):
+			if j == l - 1:
+				pass
+			elif j > l:
+				data[j] = 0
+			else:
+				data[j] = data[j] ^ data[j + 1];
+	return sock.send(data)
+			
---
+++ server.py
@@ -0,0 +1,37 @@
+import socket, sys, select, SocketServer
+import common
+class ThreadingTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer): pass
+class ProxyServer(SocketServer.StreamRequestHandler):
+	def handle_transfer(sock, remote):
+		while True:
+			r, w, e = select.select([sock, remote], [], []);
+			if sock in r:
+				if encodeSend(remote, decodeRecv(sock)) <= 0:
+					break
+			if remote in r:
+				if encodeSend(sock, decodeRecv(remote)) <= 0:
+					break
+	def handle(self):
+		try:
+			print 'In comming connection from ', self.clent_address 
+			sock = self.request
+			header = decodeRecv(sock)
+			while '\n' not in header:
+				header += decodeRecv(sock)
+			fline = tmp[0, header.find('\n')]
+			(verb, url, version) = fline.split()
+			if ':' in url:
+				(url, port) = url.split()
+			else:
+				port = '80'
+			remote = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			remote.connect((url, int(port)))
+			remote.encodeSend(remote, header)
+			handle_transfer(sock, remote)
+		except socket.error:
+			print 'Socket Error: '.socket.error
+def main():
+	server = ThreadingTCPServer(('', 5050), ProxyServer)
+	server.serve_forever
+if __name__ == '__main__':
+	main()
