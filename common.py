2f52e274cb5450144f2fb6d6e40aa9ac1827a5c4
Ming.G <mjzshd@gmail.com>
2012-09-07 12:33:00 +0800

    Bloody bug

--- common.py
+++ common.py
@@ -5,6 +5,7 @@ def decodeRecv(sock, num = 8):
 	i = num - 2
 	while i >= 0:
 		data[i] = data[i] ^ data[i + 1]
+		i -= 1
 	return ''.join(data)
 def encodeSend(sock, data, num = 8):
 	data = list(data)
@@ -14,9 +15,8 @@ def encodeSend(sock, data, num = 8):
 			if j == l - 1:
 				pass
 			elif j >= l:
-				data[j] = 0
+				data.push(0)
 			else:
 				data[j] = data[j] ^ data[j + 1];
-	data = ''.join(data)
-	return sock.send(data)
+	return sock.send(''.join(data))
 			
