86e45352fba727f9b52a48f9ce487014453999af
DavidAQ <davidaq@ubuntu.ubuntu-domain>
2012-09-12 02:03:21 +0800

    Desired test

---
+++ common.py
@@ -0,0 +1,10 @@
+import socket, struct
+swap = lambda data: chr((((ord(data) & 240) >> 4) | (ord(data) << 4)) & 255)
+def decodeRecv(sock, num = 4096):
+	data = list(sock.recv(num))
+	data = map(swap, data)
+	return ''.join(data)
+def encodeSend(sock, data):
+	data = list(data)
+	data = map(swap, data)
+	return sock.send(''.join(data))
