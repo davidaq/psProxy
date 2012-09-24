81ba0b04f2c24917443890d5920928756e539ecf
Ming.G <mjzshd@gmail.com>
2012-09-24 15:03:42 +0800

    Solve scaling problem, and add remote check

--- client.py
+++ client.py
@@ -1,17 +1,21 @@
 # Options
 
-remoteList=(
+remoteList=[
 ("127.0.0.1", 5060), # Localhost for single layer proxy
 ("184.22.246.194", 5060), # Ming's VPS
-)
+]
 
 # CODE
 
 import socket, sys, select, SocketServer, time, threading, os, traceback
 from common import *
-desireList={}
-ipList = {}
-userList = {}
+desirelist={}
+iplist = {}
+userlist = {}
+#Semaphore variable
+sema_desire = threading.BoundedSemaphore(1)
+sema_ip = threading.BoundedSemaphore(1)
+sema_user = threading.BoundedSemaphore(1)
 class ThreadingTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer): pass
 class ProxyServer(SocketServer.StreamRequestHandler):
 	def check_remote(self, linkinfo, sockshead, links):
@@ -45,49 +49,69 @@ class ProxyServer(SocketServer.StreamRequestHandler):
 			remote.close()
 		return [False, "", ""]
 	def handle_transfer(self, sock, links, ip):
-		global desireList
+		global desirelist, sema_desire
+		#Put 
 		links.append(sock)
-		ok=True
-		while ok:
+		flag = True
+		while flag:
 			r, w, e = select.select(links, [], []);
 			# forward to all active remote links
+			flag = False
 			for link in r:
-				if link is sock:
+				if link is sock:	#Client socket -- only one
 					data = sock.recv(4096)
-					if data=='':
-						ok = False
-						break
+					if len(data) == 0:
+						#print "Damn Happend!\n!\n"
+						#flag = False
+						#break
+						links.remove(sock)
+						continue
+					flag = True
 					for l in [x for x in links if x != sock]:
-						if encodeSend(l, data) <= 0:
+						if encodeSend(l, data) <= 0:  #Clear some server
 							links.remove(l)
 							if l in r: r.remove(l)
 							l.close()
-				else:
+				else:				#Client socket
 					data = decodeRecv(link, 4096)
 					if len(data) == 0: 
 						link.close()
 						links.remove(link)
 						continue
+					flag = True
+					#Close all server sockets except the ont first responced.
 					for l in [x for x in links if x != sock and x != link]:
 						l.close()
 						if l in r: r.remove(l)
-					links= [sock, link]
-					desireList[ip] = (time.time() + 3600, link.getpeername())
+					links = [sock, link]
+					
+					sema_desire.acquire()	#Semaphore, update desirelist
+					desirelist[ip] = (time.time() + 2 * 3600, link.getpeername())
+					sema_desire.release()
+					
 					if sock.send(data) <= 0:
-						ok = False
+						flag = False
 						break
+		#Close all connection
 		for link in links:
 			link.close()
+			
 	def handle(self):
-		global desireList,remoteList, ipList, userList
+		global desirelist,remoteList, iplist, userlist
+		global sema_ip, sema_desire, sema_user
 		try:
 			print "Number of threads: ", threading.activeCount()
-			if self.client_address[0] not in userList:
+			
+			sema_user.acquire()
+			if self.client_address[0] not in userlist:
 				print 'New connection from ', self.client_address 
-				userList[self.client_address[0]] = 1
+				userlist[self.client_address[0]] = 1
+			sema_user.release()
+			
 			sock = self.request			
 			sock.recv(262)
 			sock.send(b"\x05\x00")
+			time.sleep(0.1)
 			data = sock.recv(4)
 			socksHead = data
 			mode = ord(data[1])
@@ -100,18 +124,25 @@ class ProxyServer(SocketServer.StreamRequestHandler):
 				addr = sock.recv(ord(data[0])); socksHead += addr
 			socksHead += sock.recv(2); #Port
 			links=[]
+			
 			# Update desire list if exist and expire
-			if (addr in ipList and 
-				ipList[addr] in desireList and 
-				desireList[ipList[addr]][0] > time.time()):
-				flag, reply, ip = self.check_remote(desireList[ipList[addr]][1], socksHead, links)
+			sema_ip.acquire()
+			sema_desire.acquire()
+			if (addr in iplist and 
+				iplist[addr] in desirelist and 
+				desirelist[iplist[addr]][0] > time.time()):
+				flag, reply, ip = self.check_remote(desirelist[iplist[addr]][1], socksHead, links)
+			sema_ip.release()
+			sema_desire.release()
 			# Create desire list
 			if len(links) == 0:
 				for linkinfo in remoteList:
 					flag, ret, tmp_ip = self.check_remote(linkinfo, socksHead, links)
 					if flag: 	#Server support this protocol
 						reply, ip = ret, tmp_ip
-						ipList[addr] = ip
+						sema_ip.acquire()
+						iplist[addr] = ip
+						sema_ip.release()
 				if len(links) == 0:
 					print 'No usable remote proxy for ' + addr
 					sock.close()
@@ -128,6 +159,13 @@ class ProxyServer(SocketServer.StreamRequestHandler):
 			                              limit=2, file=sys.stdout)
 def main():
 	try:
+		for i in xrange(len(remoteList) - 1, -1, -1):
+			if not hostalive(remoteList[i]):
+				remoteList.remove(remoteList[i])
+		if len(remoteList) == 0:
+			print "No available server, I'm dead now"
+			return
+		print "Check remote list done, start listening..."
 		threading.stack_size(1024 * 512)
 		server = ThreadingTCPServer(('', 5070), ProxyServer)
 		server_thread = threading.Thread(target=server.serve_forever)
--- common.py
+++ common.py
@@ -8,3 +8,16 @@ def encodeSend(sock, data):
 	data = list(data)
 	data = map(swap, data)
 	return sock.send(''.join(data))
+def hostalive(sockinfo):
+	try:
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		sock.settimeout(4)  # 6 second
+		sock.connect(sockinfo)
+		print "Remote: ", sockinfo, " is Alive"
+		sock.close()
+		return True
+	except Exception:
+		print "Remote: ", sockinfo, " is Dead"
+		sock.close()
+		return False
